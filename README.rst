c00ff527b921737d1dec3a6c51b1bbebfbb42025
Scott Lau <exceedego@126.com>
2021-02-25 09:13:30 +0800

    Initial commit

---
+++ README.rst
@@ -0,0 +1,19 @@
+A personal Leet code solutions Project
+========================================
+
+This project provides some solutions for some Leet Code problems.
+
+
+Changes
+-------
+
+Version 0.0.1
+    * Initial version
+
+License
+-------
+
+The script is released under the MIT License.  The MIT License is registered
+with and approved by the Open Source Initiative [1]_.
+
+.. [1] https://opensource.org/licenses/MIT
---
+++ leetcode/stock_exchange.py
@@ -0,0 +1,40 @@
+#  The MIT License (MIT)
+#
+#  Copyright (c) 2021. Scott Lau
+#
+#  Permission is hereby granted, free of charge, to any person obtaining a copy
+#  of this software and associated documentation files (the "Software"), to deal
+#  in the Software without restriction, including without limitation the rights
+#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#  copies of the Software, and to permit persons to whom the Software is
+#  furnished to do so, subject to the following conditions:
+#
+#  The above copyright notice and this permission notice shall be included in all
+#  copies or substantial portions of the Software.
+#
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#  SOFTWARE.
+
+
+class StockExchange:
+
+    @staticmethod
+    def get_max_stock_income(arr):
+        result = 0
+        if len(arr) <= 1:
+            return -1, -1, result
+        buy_date = -1
+        sell_date = -1
+        for buy_index in range(0, len(arr) - 1):
+            for sell_index in range(buy_index + 1, len(arr)):
+                if arr[sell_index] - arr[buy_index] > result:
+                    buy_date = buy_index
+                    sell_date = sell_index
+                    result = arr[sell_index] - arr[buy_index]
+
+        return buy_date, sell_date, result
